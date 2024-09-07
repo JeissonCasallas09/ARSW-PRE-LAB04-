676a45c1eaff25c2392486f89ca7156fb51524cc
leoncico <leoncico895@hotmail.com>
2024-09-06 20:30:09 -0500

    Terminado xd

--- src/main/java/edu/eci/arsw/springdemo/GrammarChecker.java
+++ src/main/java/edu/eci/arsw/springdemo/GrammarChecker.java
@@ -3,7 +3,7 @@ package edu.eci.arsw.springdemo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
+@Service
 public class GrammarChecker {
 
 	SpellChecker sc;
@@ -15,13 +15,13 @@ public class GrammarChecker {
 		return sc;
 	}
 
+        @Autowired
 	public void setSpellChecker(SpellChecker sc) {
 		this.sc = sc;
 	}
 
 
 	public String check(String text){
-		
 		StringBuffer sb=new StringBuffer();
 		sb.append("Spell checking output:"+sc.checkSpell(text));
 		sb.append("Plagiarism checking output: Not available yet");
